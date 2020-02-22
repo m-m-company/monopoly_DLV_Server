0def53d3c0508f11361350684f9c96c88906fa92
mlori <mloria58@gmail.com>
2020-02-22 17:00:52 +0100

    Added suite class for tests

---
+++ src/test/java/com/monopoly_DLV_Server/DLV_Server/Tests.java
@@ -0,0 +1,10 @@
+package com.monopoly_DLV_Server.DLV_Server;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        BuyOrNotBuyTests.class
+})
+public class Tests{}
