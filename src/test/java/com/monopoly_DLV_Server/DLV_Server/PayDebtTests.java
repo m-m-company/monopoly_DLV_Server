b60caa05b00442cab8801205bca8845b04a10ef5
mlori <mloria58@gmail.com>
2020-03-23 17:51:13 +0100

    need fix on payDebt.dlv. Try with dlv from terminal

--- src/test/java/com/monopoly_DLV_Server/DLV_Server/PayDebtTests.java
+++ src/test/java/com/monopoly_DLV_Server/DLV_Server/PayDebtTests.java
@@ -16,7 +16,13 @@ import java.util.Comparator;
 public class PayDebtTests {
 
     ServletCalls servletCalls = new ServletCalls();
-    Comparator<ActionProperty> comparator = Comparator.comparingInt(ActionProperty::getIndex);
+    Comparator<ActionProperty> comparator = (o1, o2) -> {
+        int result = Integer.compare(o1.getIndex(), o2.getIndex());
+        if(result == 0){
+            return Integer.compare(o1.getTimes(), o2.getTimes());
+        }
+        return result;
+    };
 
     @Test
     public void sellOnlyWhatYouCan() {
@@ -28,6 +34,39 @@ public class PayDebtTests {
         String propertiesJson = JsonConverter.getInstance().toJson(properties);
         ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -200);
         ArrayList<ActionProperty> expected = new ArrayList<>(Arrays.asList(new ActionProperty(1, 0), new ActionProperty(2,0)));
+        result.sort(comparator);
+        expected.sort(comparator);
+        for(int i = 0; i < result.size(); ++i){
+            Assert.assertEquals(expected.get(i), result.get(i));
+        }
+        if (result.size() == 0){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void reachMoney() {
+        Property p1 = new Property("name", 0, "false", 0, 0, 1, 200, 0, 1);
+        ArrayList<Property> properties = new ArrayList<Property>(Collections.singletonList(p1));
+        String propertiesJson = JsonConverter.getInstance().toJson(properties);
+        ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -100);
+        ArrayList<ActionProperty> expected = new ArrayList<>(Collections.singletonList(new ActionProperty(1, 0)));
+        for(int i = 0; i < result.size(); ++i){
+            Assert.assertEquals(expected.get(i), result.get(i));
+        }
+        if (result.size() == 0){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void recursiveSell(){
+        Property p1 = new Property("name", 0, "false", 1, 0, 3, 100, 0, 1);
+        Property p2 = new Property("name", 0, "false", 1, 0, 3, 100, 0, 2);
+        ArrayList<Property> properties = new ArrayList<Property>(Arrays.asList(p1, p2));
+        String propertiesJson = JsonConverter.getInstance().toJson(properties);
+        ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -100);
+        ArrayList<ActionProperty> expected = new ArrayList<>(Arrays.asList(new ActionProperty(1, 1), new ActionProperty(2,1), new ActionProperty(2, 0)));
         result.sort(comparator);
         expected.sort(comparator);
         for(int i = 0; i < result.size(); ++i){
@@ -59,12 +98,19 @@ public class PayDebtTests {
     }
 
     @Test
-    public void reachMoney() {
-        Property p1 = new Property("name", 0, "false", 0, 0, 1, 200, 0, 1);
-        ArrayList<Property> properties = new ArrayList<Property>(Collections.singletonList(p1));
+    public void preferPropertyWithoutGroup() {
+        Property p1 = new Property("name", 0, "false", 0, 0, 3, 100, 0, 1);
+        Property p2 = new Property("name", 0, "false", 0, 0, 4, 100, 0, 2);
+        Property p3 = new Property("name", 0, "false", 0, 0, 4, 100, 0, 3);
+        Property p4 = new Property("name", 0, "false", 0, 0, 4, 100, 0, 4);
+        Property p5 = new Property("name", 0, "false", 0, 0, 5, 100, 0, 5);
+        Property p6 = new Property("name", 0, "false", 0, 0, 5, 100, 0, 6);
+        ArrayList<Property> properties = new ArrayList<Property>(Arrays.asList(p1, p2, p3, p4, p5, p6));
         String propertiesJson = JsonConverter.getInstance().toJson(properties);
-        ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -100);
-        ArrayList<ActionProperty> expected = new ArrayList<>(Collections.singletonList(new ActionProperty(1, 0)));
+        ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -150);
+        ArrayList<ActionProperty> expected = new ArrayList<>(Arrays.asList(new ActionProperty(1, 0), new ActionProperty(5,0), new ActionProperty(6, 0)));
+        result.sort(comparator);
+        expected.sort(comparator);
         for(int i = 0; i < result.size(); ++i){
             Assert.assertEquals(expected.get(i), result.get(i));
         }
@@ -84,6 +130,24 @@ public class PayDebtTests {
         String propertiesJson = JsonConverter.getInstance().toJson(properties);
         ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -100);
         ArrayList<ActionProperty> expected = new ArrayList<>(Arrays.asList(new ActionProperty(3, 1), new ActionProperty(4,2), new ActionProperty(5,1)));
+        result.sort(comparator);
+        expected.sort(comparator);
+        for(int i = 0; i < result.size(); ++i){
+            Assert.assertEquals(expected.get(i), result.get(i));
+        }
+        if (result.size() == 0){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void preferPropertyHighRent() {
+        Property p1 = new Property("name", 0, "false", 0, 0, 3, 100, 50, 1);
+        Property p2 = new Property("name", 0, "false", 0, 0, 4, 100, 40, 2);
+        ArrayList<Property> properties = new ArrayList<Property>(Arrays.asList(p1, p2));
+        String propertiesJson = JsonConverter.getInstance().toJson(properties);
+        ArrayList<ActionProperty> result = servletCalls.payDebt(propertiesJson, -50);
+        ArrayList<ActionProperty> expected = new ArrayList<>(Collections.singletonList(new ActionProperty(2, 0)));
         result.sort(comparator);
         expected.sort(comparator);
         for(int i = 0; i < result.size(); ++i){
