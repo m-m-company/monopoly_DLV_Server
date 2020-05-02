03d6f9435b17fd4750009ca6d92759c74146a9e1
MattNot <matteoatnotaro@gmail.com>
2020-05-02 17:59:31 +0200

    Test on AuctionTests.java, improvements to bid.dlv

--- src/test/java/com/monopoly_DLV_Server/DLV_Server/AuctionTests.java
+++ src/test/java/com/monopoly_DLV_Server/DLV_Server/AuctionTests.java
@@ -60,7 +60,7 @@ public class AuctionTests {
         Player player4 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 3, false, "dlv");
         ArrayList<Player> players = new ArrayList<>(Arrays.asList(player1, player2, player3, player4));
         String playersJson = JsonConverter.getInstance().toJson(players);
-        Integer result = this.servletCalls.auction(playersJson, propertiesJson, 0, 20, 0, 2);
+        Integer result = this.servletCalls.auction(playersJson, propertiesJson, 0, 20, 0, 0);
         Assert.assertTrue(result > 0);
     }
 
@@ -108,4 +108,47 @@ public class AuctionTests {
         Assert.assertTrue(result > 0);
     }
 
+    @Test
+    public void offerHighestBidGreaterThan0() {
+        Property property1 = new Property("name", 0, "false", 0, 0, 5, 250, 0, 0);
+        Property property3 = new Property("name", 1, "false", 0, 0, 1, 10, 0, 3);
+        Property property4 = new Property("name", 2, "false", 0, 0, 1, 10, 0, 4);
+        Property property5 = new Property("name", 3, "false", 0, 0, 1, 10, 0, 5);
+        Property property6 = new Property("name", 1, "false", 0, 0, 1, 10, 0, 6);
+        Property property7 = new Property("name", 2, "false", 0, 0, 1, 10, 0, 7);
+        Property property8 = new Property("name", 0, "false", 0, 0, 1, 10, 0, 8);
+
+        ArrayList<Property> properties = new ArrayList<>(Arrays.asList(property1, property3, property4, property5, property6, property7, property8));
+        String propertiesJson = JsonConverter.getInstance().toJson(properties);
+        Player player1 = new Player("name", "", 0, 1200, 0, "false", 0, false, false, "true", 0, false, "dlv");
+        Player player2 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 1, false, "dlv");
+        Player player3 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 2, false, "dlv");
+        Player player4 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 3, false, "dlv");
+        ArrayList<Player> players = new ArrayList<>(Arrays.asList(player1, player2, player3, player4));
+        String playersJson = JsonConverter.getInstance().toJson(players);
+        Integer result = this.servletCalls.auction(playersJson, propertiesJson, 0, 50, 0, 2);
+        Assert.assertTrue(result > 0);
+    }
+
+    @Test
+    public void imInterestedInThisOne() {
+        Property property1 = new Property("name", 0, "false", 0, 0, 1, 10, 0, 0);
+        Property property3 = new Property("name", 1, "false", 0, 0, 1, 10, 0, 3);
+        Property property4 = new Property("name", 2, "false", 0, 0, 1, 10, 0, 4);
+        Property property5 = new Property("name", 3, "false", 0, 0, 1, 10, 0, 5);
+        Property property6 = new Property("name", 1, "false", 0, 0, 1, 10, 0, 6);
+        Property property7 = new Property("name", 2, "false", 0, 0, 1, 10, 0, 7);
+        Property property8 = new Property("name", 0, "false", 0, 0, 1, 10, 0, 8);
+
+        ArrayList<Property> properties = new ArrayList<>(Arrays.asList(property1, property3, property4, property5, property6, property7, property8));
+        String propertiesJson = JsonConverter.getInstance().toJson(properties);
+        Player player1 = new Player("name", "", 0, 1200, 0, "false", 0, false, false, "true", 0, false, "dlv");
+        Player player2 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 1, false, "dlv");
+        Player player3 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 2, false, "dlv");
+        Player player4 = new Player("name", "", 0, 300, 0, "false", 0, false, false, "true", 3, false, "dlv");
+        ArrayList<Player> players = new ArrayList<>(Arrays.asList(player1, player2, player3, player4));
+        String playersJson = JsonConverter.getInstance().toJson(players);
+        Integer result = this.servletCalls.auction(playersJson, propertiesJson, 0, 50, 0, 2);
+        Assert.assertTrue(result > 0);
+    }
 }
